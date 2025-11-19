ad7ef7d3201d870e89c04aa3ccdc34278bc2da48
Zaid Malik <mzaidmalik25@gmail.com>
2025-11-19 14:59:34 +0530

    feat: create user api this api fetch the current login user data

---
+++ src/app/api/user/route.ts
@@ -0,0 +1,39 @@
+import { authOptions } from "@/libs/auth";
+import { connectDb } from "@/libs/db";
+import User from "@/models/user.model";
+import { getServerSession } from "next-auth";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(req: NextRequest) {
+  try {
+    await connectDb();
+    const session = await getServerSession(authOptions);
+
+    if (!session?.user?.email || !session?.user?.id) {
+      return NextResponse.json(
+        { message: "User does not have any session" },
+        { status: 400 }
+      );
+    }
+
+   const user = await User.findById(session.user.id).select("-password")
+   if(!user){
+    return NextResponse.json(
+        { message: "User is not found" },
+        { status: 400 }
+      );
+   }
+
+   return NextResponse.json(
+        user,
+        { status: 200 }
+      );
+
+
+  } catch (error) {
+    return NextResponse.json(
+        error,
+        { status: 500 }
+      );
+  }
+}
